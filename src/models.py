85c3a099045555eb10576bc8e1fc6f77535f9bfa
angelicamaliqueo <angelicamaliqueor@gmail.com>
2022-08-20 02:37:54 +0000

    tablas

--- src/models.py
+++ src/models.py
@@ -8,12 +8,14 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
+
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+
 
 class Address(Base):
     __tablename__ = 'address'
@@ -26,8 +28,46 @@ class Address(Base):
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
+
+class Personajes(Base):
+    __tablename__ = 'personajes'
+    id = Column(Integer, primary_key=True)
+    personajes = Column(String(250))
+    personajes_id = Column(Integer, ForeignKey('user.id'))
+    personajesfavorito_id = Column(Integer, ForeignKey('favoritos.id'))
+
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    id = Column(Integer, primary_Key=True)
+    planetas = Column(String(250))
+    clima = Column(String(250))
+    planetas_id = Column(Integer, ForeignKey('user.id'))
+    planetasfavoritos_id = Column(Integer, ForeignKey('favoritos.id'))
+
+   
+   
+    class Tiposplanetas(Base):
+        __tablename__ = 'tiposplanetas'
+        id = Column(Integre, primary_Key=True)
+        planetas = Column(String(250))
+        tiposplanetas = Column(String(250))
+        tiposplanetas_id = Column(Integer, ForeignKey('planetas.id'))
+
+
+class Naves(Base):
+    __tablename__ = 'naves'
+    id = Column(Integer, primary_Key=True)
+    nave = Column(String(250))
+    nave_id = Column(Integer, ForeignKey('user.id'))
+    navefavoritos_id = Column(Integer, ForeignKey('favoritos.id'))
+
+   
+   
+   
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+
+# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
